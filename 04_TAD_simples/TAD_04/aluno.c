e4a1d6be52ac2c172f1da4771c8c6e6fb106953e
GabrielFeaR <gabrieldealbuquerque1@gmail.com>
2023-09-21 13:46:47 -0300

    a

---
+++ 04_TAD_simples/TAD_04/aluno.c
@@ -0,0 +1,41 @@
+#include <stdio.h>
+#include "aluno.h"
+
+tAluno CriaAluno(char *nome, int matricula, int n1, int n2, int n3)
+{
+
+}
+
+
+tAluno LeAluno()
+{
+    int qtdAlunos, matricula, i, j;
+    char nome[46];
+    scanf("%d, qtdAlunos");
+    for (i = 0; i < qtdAlunos; i++)
+    {
+        
+    }
+}
+
+
+int ComparaMatricula(tAluno aluno1, tAluno aluno2)
+{
+
+}
+
+int CalculaMediaAluno(tAluno aluno)
+{
+
+}
+
+
+int VerificaAprovacao(tAluno aluno);
+{
+
+}
+
+void ImprimeAluno(tAluno aluno);
+{
+
+}
